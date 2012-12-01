5346ef1e4904d6f78b70cbed5ccf76af6a9e53e7
Ping-Chris Zheng <christophercheng1222@gmail.com>
2012-12-01 23:53:01 +0800

    initial files

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Fizz Buzz function
+ * 
+ * @author   Ping-Chris Zheng <christophercheng1222@gmail.com> 
+ * @since    Dec 01, 2012
+ */
+
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Description PropertyGuru Test Task1 Index page
+ * 
+ * @author   Ping-Chris Zheng <christophercheng1222@gmail.com> 
+ * @since    Dec 01, 2012
+ */
+?>
+
