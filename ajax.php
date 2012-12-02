5d3babea2e689e78777455c137c3e553033d5784
Ping-Chris Zheng <christophercheng1222@gmail.com>
2012-12-02 11:35:02 +0800

    added the validation function

--- ajax.php
+++ ajax.php
@@ -11,5 +11,22 @@ include_once 'functions.php';
 
 $data = $_POST;
 
+// Validate the start and end number first
+$errors = array();
+if(!preg_match('/^[1-9][0-9]*$/',$data['start']))
+{
+    $errors[] = 'Invalid start number';
+}
+if(!preg_match('/^[1-9][0-9]*$/',$data['end']))
+{
+    $errors[] = 'Invalid end number';
+}
+
+
+if($data['function'] == 'fizzbuzz')
+{
+    
+}
+
 echo json_encode($data);
 ?>
